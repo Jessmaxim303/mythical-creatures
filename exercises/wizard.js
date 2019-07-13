e0ebcee500f7a988f345e325ee875c0a52402c0d
Jesse Maxim <jessmaxim303@gmail.com>
2019-07-12 22:27:27 -0600

    Completed wizard

--- exercises/wizard.js
+++ exercises/wizard.js
@@ -1,11 +1,42 @@
 class Wizard {
-	constructor(name) {
-		this.name = name;
-		this.beard = false;
-	}
+	constructor(object) {
+		this.name = object.name;
+		this.bearded = true;
+		this.isRested = true;
+		this.castSpells = 0;
+
+		if (object.bearded !== undefined) {
+			this.bearded = object.bearded;
+		}
 }
+		incantation(spells) {
+			return spells.toUpperCase();
+		}
+
+		cast(castSpells) {
+			 this.castSpells++
+    if(this.castSpells >= 3){
+      this.isRested = false;
+      return "I SHALL NOT CAST!";
+    }else{
+      return "MAGIC BULLET";
+    } 
+  }
+
+    		
+};
 
 module.exports = Wizard;
 
 
 // npm test mythical-creatures/test/wizard-test.js
+
+
+
+
+// if (job === undefined) {
+// 			return this.job = 'Scallywag';
+// 		} else {
+// 			this.job = job;
+// 		}
+// 	}
